a6780a354e5952ef75d6b8db30231513c6eed017
jkelly088 <Joseph_kelly@comcast.com>
2021-07-14 12:07:36 -0400

    Replace CASETLVTags with TLV::ContextTag ID, switch unsafe statement to an actual OperationalCredentialSet method to retrieve a TrustedRootId, renamed kMAX_Hash_Length refs to kSHA256_Hash_Length to match the Spec.

--- src/controller/CHIPDevice.cpp
+++ src/controller/CHIPDevice.cpp
@@ -530,8 +530,8 @@ CHIP_ERROR Device::WarmupCASESession()
     mLocalMessageCounter = 0;
     mPeerMessageCounter  = 0;
 
-    ReturnErrorOnFailure(
-        mCASESession.EstablishSession(mDeviceAddress, mCredentials, *mCredentialsIndex, mDeviceId, keyID, exchange, this));
+    ReturnErrorOnFailure(mCASESession.EstablishSession(mDeviceAddress, mCredentials, *mCredentialsIndex, mDeviceId, mDeviceFabricId,
+                                                       keyID, exchange, this));
 
     mState = ConnectionState::Connecting;
 
